965d03c7ab5be6d7da4e5e12a1cb73e2965293a2
vmotasilva <49719510+vmotasilva@users.noreply.github.com>
2025-11-21 10:58:32 -0300

    recriando script de admin e enviando para producao

--- create_admin.py
+++ create_admin.py
@@ -1,18 +1,12 @@
 import os
 import django
+from django.contrib.auth import get_user_model
 
-# Configura o Django para rodar o script
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
-from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
-# Só cria se não existir
 if not User.objects.filter(username='admin').exists():
-    print("Criando usuario admin...")
     User.objects.create_superuser('admin', 'admin@exemplo.com', 'admin123')
-    print("Pronto! Usuario criado.")
-else:
-    print("Usuario admin ja existe.")
+    print("ADMIN CRIADO COM SUCESSO")
