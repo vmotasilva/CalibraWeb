fe041626dd90de5977a6f900c016e697a6f18553
vmotasilva <49719510+vmotasilva@users.noreply.github.com>
2025-11-24 10:31:25 -0300

    Corrige nomes das rotas de template no urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,6 @@ urlpatterns = [
     # 4. Dashboard
     path('home/', views.dashboard_view, name='home'),
 
-    path('solicitacao/nova/', views.nova_solicitacao, name='nova_solicitacao'),
-    path('instrumento/<int:instrumento_id>/', views.detalhe_instrumento_view, name='detalhe_instrumento'),
-
     # 5. Módulos Principais
     path('metrologia/', views.modulo_metrologia_view, name='modulo_metrologia'),
     path('rh/', views.modulo_rh_view, name='modulo_rh'),
@@ -32,17 +29,18 @@ urlpatterns = [
     path('rh/editar/<int:colab_id>/', views.editar_colaborador_view, name='editar_colaborador'),
     path('metrologia/instrumento/<int:instrumento_id>/', views.detalhe_instrumento_view, name='detalhe_instrumento'),
 
+    # --- NOVA ROTA PARA SOLICITAÇÕES ---
+    path('solicitacao/nova/', views.nova_solicitacao, name='nova_solicitacao'),
+
     # 6. Funcionalidades Específicas
-    # CORREÇÃO: O NOME VOLTOU A SER 'carimbar' PARA NÃO QUEBRAR O MENU
-    path('carimbar/', views.carimbar_view, name='carimbar'),
-    
+    path('carimbar/', views.carimbar_view, name='carimbar'), 
     path('metrologia/historico/remover/<int:historico_id>/', views.remover_historico_view, name='remover_historico'),
     
-    # 7. Downloads de Templates
-    path('dl-template-inst/', views.dl_template_instr, name='dl_template_instr'),
-    path('dl-template-colab/', views.dl_template_colab, name='dl_template_colab'),
-    path('dl-template-hier/', views.dl_template_hierarquia, name='dl_template_hierarquia'),
-    path('dl-template-hist/', views.dl_template_historico, name='dl_template_historico'),
+    # 7. Downloads de Templates (CORRIGIDO: Os nomes agora batem com o HTML)
+    path('dl-template-inst/', views.dl_template_instr, name='template_instrumentos'),
+    path('dl-template-colab/', views.dl_template_colab, name='template_colaboradores'), # <--- CORRIGIDO AQUI
+    path('dl-template-hier/', views.dl_template_hierarquia, name='template_hierarquia'),
+    path('dl-template-hist/', views.dl_template_historico, name='template_historico'),
     path('dl-template-ferias/', views.dl_template_ferias, name='dl_template_ferias'),
 
     # 8. Importações (Excel)
