f191a7618709f7c49cb7ff0ccd916aef5f039661
vmotasilva <vmotasilva@gmail.com>
2025-11-23 23:16:50 -0300

    Ajuste de Filtros metrologia

--- config/urls.py
+++ config/urls.py
@@ -23,33 +23,32 @@ urlpatterns = [
     # 5. Módulos Principais
     path('metrologia/', views.modulo_metrologia_view, name='modulo_metrologia'),
     path('rh/', views.modulo_rh_view, name='modulo_rh'),
+    
+    # Detalhes e Edição
     path('rh/colaborador/<int:colab_id>/', views.detalhe_colaborador_view, name='detalhe_colaborador'),
     path('rh/editar/<int:colab_id>/', views.editar_colaborador_view, name='editar_colaborador'),
+    path('metrologia/instrumento/<int:instrumento_id>/', views.detalhe_instrumento_view, name='detalhe_instrumento'),
 
     # 6. Funcionalidades Específicas
-    path('carimbar/', views.carimbar_view, name='carimbo'),
-    path('detalhe/<int:instrumento_id>/', views.detalhe_instrumento_view, name='detalhe_instrumento'),
+    # CORREÇÃO AQUI: O nome agora é 'carimbo' para bater com o menu novo
+    path('carimbo/', views.carimbar_view, name='carimbo'),
+    
+    path('metrologia/historico/remover/<int:historico_id>/', views.remover_historico_view, name='remover_historico'),
     
     # 7. Downloads de Templates
-    path('dl-template-inst/', views.dl_template_instr, name='template_instrumentos'),
-    path('dl-template-colab/', views.dl_template_colab, name='template_colaboradores'),
-    path('dl-template-hier/', views.dl_template_hierarquia, name='template_hierarquia'),
-    path('dl-template-hist/', views.dl_template_historico, name='template_historico'),
-    
-    # --- NOVO: Template de Férias (Correção do Erro) ---
+    path('dl-template-inst/', views.dl_template_instr, name='dl_template_instr'),
+    path('dl-template-colab/', views.dl_template_colab, name='dl_template_colab'),
+    path('dl-template-hier/', views.dl_template_hierarquia, name='dl_template_hierarquia'),
+    path('dl-template-hist/', views.dl_template_historico, name='dl_template_historico'),
     path('dl-template-ferias/', views.dl_template_ferias, name='dl_template_ferias'),
 
-    # 8. Importações
+    # 8. Importações (Excel)
     path('imp-inst/', views.imp_instr_view, name='importar_instrumentos'),
     path('imp-colab/', views.imp_colab_view, name='importar_colaboradores'),
     path('imp-hist/', views.imp_historico_view, name='importar_historico'),    
     path('imp-hierarquia/', views.imp_hierarquia_view, name='importar_hierarquia'),
-    
-    # --- NOVO: Importação de Férias (Correção do Erro) ---
     path('imp-ferias/', views.imp_ferias_view, name='importar_ferias'),
-
-    # ... outras urls ...
-    path('remover-historico/<int:historico_id>/', views.remover_historico_view, name='remover_historico'),
+    path('imp-padroes/', views.imp_padroes_view, name='importar_padroes'),
 ]
 
 # Configuração para servir arquivos de mídia/estáticos em modo Debug
