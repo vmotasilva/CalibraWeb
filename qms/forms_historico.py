cc82c0620acf6a1e1b9537c5121919110316830c
vmotasilva <vmotasilva@gmail.com>
2025-12-16 17:49:48 -0300

    fix: Remover atributo multiple do FileInput que causa ValueError

--- qms/forms_historico.py
+++ qms/forms_historico.py
@@ -29,7 +29,6 @@ class HistoricoCalibracaoForm(forms.ModelForm):
         widget=forms.FileInput(attrs={
             'class': 'form-control form-control-sm',
             'accept': '.pdf',
-            'multiple': True,
         })
     )
     
