8a58aebd04279166ad02ee0c1ab6707a54befb16
vmotasilva <49719510+vmotasilva@users.noreply.github.com>
2026-07-21 16:00:39 -0300

    fix: adicionar importação de require_POST

--- auditoria/views.py
+++ auditoria/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from django.core import signing
 from django.db import models, transaction
 from django.db.models.deletion import ProtectedError
