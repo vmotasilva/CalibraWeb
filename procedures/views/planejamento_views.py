fb915cd5c2198281692f7b972bdcd3156c0dc01e
Vinicius Mota <vmota@calibra.com>
2026-08-27 22:12:01 -0300

    fix(procedures): importar require_GET e require_POST em planejamento_views

--- procedures/views/planejamento_views.py
+++ procedures/views/planejamento_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET, require_POST, require_http_methods
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db import transaction
